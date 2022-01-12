9a6606110768a73b5bf2a212c1137e6806434141
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-12 16:37:21 -0300

    feat:added validation on routes

--- src/api/routes/RouteCity.ts
+++ src/api/routes/RouteCity.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { CityController } from '../controller/CityController';
+import validCreate from '../validation/cityValidation/create';
+import validGet from '../validation/cityValidation/getAll';
+import validUpdate from '../validation/cityValidation/update';
+import validId from '../validation/getByidValid';
 
 const cityRouter = Router();
 
-cityRouter.post('/cities', new CityController().create);
-cityRouter.get('/cities', new CityController().findAll);
-cityRouter.get('/cities/:id', new CityController().getById);
-cityRouter.put('/cities/:id', new CityController().update);
+cityRouter.post('/cities', validCreate, new CityController().create);
+cityRouter.get('/cities', validGet, new CityController().findAll);
+cityRouter.get('/cities/:id', validId, new CityController().getById);
+cityRouter.put('/cities/:id', validUpdate, new CityController().update);
 cityRouter.delete('/cities/:id', new CityController().delete);
 export { cityRouter };
