5c15d313af46e7babbb8914843e7097eac31bf28
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-13 11:42:13 -0300

    created validation placed on routes, get, create, post, getbyid and update.

--- src/api/routes/routeClient.ts
+++ src/api/routes/routeClient.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { ClientController } from '../controller/ClientController';
+import validCreate from '../validation/clientValidation/create';
+import validGetAll from '../validation/clientValidation/getAll';
+import validUpdate from '../validation/clientValidation/update';
+import validGetById from '../validation/getByidValid';
 
 const clientRouter = Router();
 
-clientRouter.post('/clients', new ClientController().create);
-clientRouter.get('/clients', new ClientController().findAll);
-clientRouter.get('/clients/:id', new ClientController().getById);
-clientRouter.put('/clients/:id', new ClientController().update);
-clientRouter.delete('/clients/:id', new ClientController().delete);
+clientRouter.post('/clients', validCreate, new ClientController().create);
+clientRouter.get('/clients', validGetAll, new ClientController().findAll);
+clientRouter.get('/clients/:id', validGetById, new ClientController().getById);
+clientRouter.put('/clients/:id', validUpdate, validGetById, new ClientController().update);
+clientRouter.delete('/clients/:id', validGetById, new ClientController().delete);
 export { clientRouter };
