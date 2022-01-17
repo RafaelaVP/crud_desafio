e73aa778851168fd61eaca4535c6c4007e6299b7
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-17 01:10:07 -0300

    refatory: adjust post validation to check between valid states in the city and to check customer by gender

--- src/app/validation/cityValidation/create.ts
+++ src/app/validation/cityValidation/create.ts
@@ -3,8 +3,39 @@ import Joi from 'joi';
 export = async (req, res, next) => {
   try {
     const city = Joi.object({
-      city: Joi.string().min(2).max(30).required(),
-      state: Joi.string().min(2).max(30).required()
+      city: Joi.string().trim().required(),
+      state: Joi.string()
+        .trim()
+        .required()
+        .valid(
+          'AC',
+          'AL',
+          'AP',
+          'AM',
+          'BA',
+          'CE',
+          'DF',
+          'ES',
+          'GO',
+          'MA',
+          'MT',
+          'MS',
+          'MG',
+          'PA',
+          'PB',
+          'PR',
+          'PE',
+          'PI',
+          'RJ',
+          'RN',
+          'RS',
+          'RO',
+          'RR',
+          'SC',
+          'SP',
+          'SE',
+          'TO'
+        )
     });
 
     const { error } = await city.validate(req.body, { abortEarly: false });
