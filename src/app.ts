fff7e45228943998bd7cfb27601f4711ada75146
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-17 01:00:25 -0300

    feat: created route for swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 import './infra/database';
 import { routes } from './app/routes';
 
@@ -9,5 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export { app };
