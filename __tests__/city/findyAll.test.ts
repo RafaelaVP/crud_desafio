5c3b42961cdc98dbbdbdd32800b83140c23a19cc
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-17 01:15:30 -0300

    refatory:tried to hit create test.

---
+++ __tests__/city/findyAll.test.ts
@@ -0,0 +1,21 @@
+import { request } from 'supertest';
+import { app } from '../../src/app';
+
+describe('listar todos os clientes', () => {
+  it('retornar status 200', async () => {
+    const cityMock = {
+      city: 'Pelotas',
+      state: 'RS'
+    };
+
+    await request(app).post('/api/cities/').send(cityMock);
+
+    const response = await request(app).get('/api/cities/');
+    const { body } = response;
+    const { users } = body;
+    expect(users[0].city).toBe(cityMock.city);
+    expect(users[0].state).toBe(cityMock.state);
+    const { status } = response;
+    expect(status).toBe(200);
+  });
+});
