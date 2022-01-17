fb5487c9bff48af641d99ed2da46c5af362528de
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-17 01:05:39 -0300

    feat: entered search route by name.

--- src/app/routes/routeClient.ts
+++ src/app/routes/routeClient.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { ClientController } from '../controller/ClientController';
-// import validCreate from '../validation/clientValidation/create';
+import validCreate from '../validation/clientValidation/create';
 import validGetAll from '../validation/clientValidation/getAll';
-// import validUpdate from '../validation/clientValidation/update';
+import validUpdate from '../validation/clientValidation/update';
 import validGetById from '../validation/getByidValid';
 
 const clientRouter = Router();
 
-clientRouter.post('/api/clients', new ClientController().create);
+clientRouter.post('/api/clients', validCreate, new ClientController().create);
 clientRouter.get('/api/clients', validGetAll, new ClientController().findAll);
 clientRouter.get('/api/clients/:id', validGetById, new ClientController().getById);
-clientRouter.put('/api/clients/:id', new ClientController().update);
-clientRouter.delete('/api/clients/:id', new ClientController().delete);
+clientRouter.put('/api/clients/:id', validUpdate, new ClientController().update);
+clientRouter.delete('/api/clients/:id', validGetAll, new ClientController().delete);
 clientRouter.get('/api/clients/name/:name', new ClientController().getByName);
 export { clientRouter };
