bfa87e8e1ee47eceebc45ade7a99973c5f33da60
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-17 16:16:31 -0300

    refatory: tried typing.

--- src/app/repository/BaseRepository.ts
+++ src/app/repository/BaseRepository.ts
@@ -16,31 +16,35 @@ export class BaseRepository {
     return result;
   }
 
-  async findOne(_id): Promise<any | Error> {
+  async findOne(_id: string): Promise<any | Error> {
     const result = await getConnection(process.env.NODE_ENV).getRepository(this.entity).findOne(_id);
     return result;
   }
 
-  async update(_id, payload) {
+  async update(_id: string, payload) {
     return getConnection(process.env.NODE_ENV).getRepository(this.entity).update(_id, payload);
   }
 
-  async delete(_id) {
+  async delete(_id: string) {
     return getConnection(process.env.NODE_ENV).getRepository(this.entity).delete(_id);
   }
 
-  async findByCity(city): Promise<any | Error> {
+  async findByCity(city: string): Promise<any | Error> {
     const result = await getConnection(process.env.NODE_ENV).getRepository(this.entity).findOne(city);
     return result;
   }
 
-  async findByState(state): Promise<any | Error> {
+  async findByState(state: string): Promise<any | Error> {
     const result = await getConnection(process.env.NODE_ENV).getRepository(this.entity).findOne(state);
     return result;
   }
 
-  async findByNameClient(name): Promise<any | Error> {
+  async findByNameClient(name: string): Promise<any | Error> {
     const result = await getConnection(process.env.NODE_ENV).getRepository(this.entity).findOne(name);
     return result;
+  }
+
+  async updateName(_id: string, name: string) {
+    return getConnection(process.env.NODE_ENV).getRepository(this.entity).update(_id, name);
   }
 }
