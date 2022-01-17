1bc664340d463fee646c84df60b3e50d60f48d59
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-17 01:11:11 -0300

    refatory: tried to resolve the query error by entering typing without success

--- src/app/types/city/RequestCreateCity.ts
+++ src/app/types/city/RequestCreateCity.ts
@@ -2,9 +2,15 @@ export class CityRequest {
   constructor() {
     this.city = undefined;
     this.state = undefined;
+    this.page = undefined;
+    this.limit = undefined;
   }
 
   city: string;
 
   state: string;
+
+  page: number;
+
+  limit: number;
 }
