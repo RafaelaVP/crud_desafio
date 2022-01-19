58e07bb909035f4721bb7c78a8f16015e5573636
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-19 18:32:34 -0300

    feat:created an interface to type the findAll that had an error, updated the name of the folder for the interface and removed the request file that was incorrect.

---
+++ src/app/interfaces/InterfaceCity.ts
@@ -0,0 +1,12 @@
+export interface Icities {
+  docs: [
+    {
+      city: string,
+      state: string
+    }
+  ]
+
+  limit: number;
+  offset: number;
+  total: number;
+}
---
+++ src/app/interfaces/InterfaceClient.ts
@@ -0,0 +1,22 @@
+export interface Iclients {
+  docs: [
+    {
+      name:string,
+      gender: string,
+      birthdate: Date,
+      city_home: {
+        id:string,
+        city: string,
+        state: string
+      }
+      age:number
+
+      
+
+    }
+  ]
+
+  limit: number;
+  offset: number;
+  total: number;
+}
