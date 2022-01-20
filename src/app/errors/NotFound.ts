44f398800275319824e96b85d2cb3da07b468851
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-20 17:32:03 -0300

    refatory: adjusted set to just not found

---
+++ src/app/errors/NotFound.ts
@@ -0,0 +1,12 @@
+export class NotFound extends Error {
+    statusCode: number;
+  
+    description: string;
+  
+    constructor(id) {
+      super();
+      this.statusCode = 404;
+      this.description = 'Not found';
+      this.message = `The ID: ${id} was not found`;
+    }
+  }
