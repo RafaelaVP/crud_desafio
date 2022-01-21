069b3c895f71d484b7d40b1f1e1e9f17b54621ca
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-21 09:33:56 -0300

    refatory: identified that getAll validation was not being validated because it is already coming because the are validated correctly removed routes

--- src/app/routes/routeClient.ts
+++ src/app/routes/routeClient.ts
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import { ClientController } from '../controller/ClientController';
 import validCreate from '../validation/clientValidation/create';
-import validGetAll from '../validation/clientValidation/getAll';
+
 import validUpdate from '../validation/clientValidation/update';
 import validGetById from '../validation/getByidValid';
 
 const clientRouter = Router();
 
 clientRouter.post('/api/clients', validCreate, new ClientController().create);
-clientRouter.get('/api/clients', validGetAll, new ClientController().findAll);
+clientRouter.get('/api/clients',  new ClientController().findAll);
 clientRouter.get('/api/clients/:id', validGetById, new ClientController().getById);
 clientRouter.put('/api/clients/:id', validUpdate, new ClientController().update);
-clientRouter.delete('/api/clients/:id', validGetAll, new ClientController().delete);
+clientRouter.delete('/api/clients/:id', new ClientController().delete);
 
 export { clientRouter };
