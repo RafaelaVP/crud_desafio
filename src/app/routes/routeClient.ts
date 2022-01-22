427d4719f0a8de4057dea4b4407ee75776688c2e
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-21 21:39:34 -0300

    refatory:adjust routes client

--- src/app/routes/routeClient.ts
+++ src/app/routes/routeClient.ts
@@ -1,15 +1,14 @@
 import { Router } from 'express';
 import { ClientController } from '../controller/ClientController';
 import validCreate from '../validation/clientValidation/create';
-
 import validUpdate from '../validation/clientValidation/update';
-import validGetById from '../validation/getByidValid';
+
 
 const clientRouter = Router();
 
 clientRouter.post('/api/clients', validCreate, new ClientController().create);
 clientRouter.get('/api/clients',  new ClientController().findAll);
-clientRouter.get('/api/clients/:id', validGetById, new ClientController().getById);
+clientRouter.get('/api/clients/:id', new ClientController().getById);
 clientRouter.put('/api/clients/:id', validUpdate, new ClientController().update);
 clientRouter.delete('/api/clients/:id', new ClientController().delete);
 
