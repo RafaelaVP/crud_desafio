0f3f85ee6560f6b9875f275d22cc851a448f309f
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-24 17:12:16 -0300

    refatory: adjust the name of the route that was capitalized incorrectly because it is not a class.

--- src/app/routes/routeCity.ts
+++ src/app/routes/routeCity.ts
@@ -8,13 +8,9 @@ import validId from '../validation/getByidValid';
 const cityRouter = Router();
 
 cityRouter.post('/api/cities', validCreate, new CityController().create);
-
 cityRouter.get('/api/cities', validGetAll, new CityController().findAll);
-
 cityRouter.get('/api/cities/:id', validId, new CityController().getById);
-
 cityRouter.put('/api/cities/:id', validUpdate, validId, new CityController().update);
-
 cityRouter.delete('/api/cities/:id', validId, new CityController().delete);
 
 export { cityRouter };
