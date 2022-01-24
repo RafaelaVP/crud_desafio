08e8272f0cb669f162698ff3e6415f29a0d5bece
Rafaela Pintado Valerio <rafavalpint@gmail.com>
2022-01-24 14:38:46 -0300

    refatory: add validate getAll

--- src/app/routes/RouteCity.ts
+++ src/app/routes/RouteCity.ts
@@ -1,14 +1,14 @@
 import { Router } from 'express';
 import { CityController } from '../controller/CityController';
 import validCreate from '../validation/cityValidation/create';
-
+import validGetAll from '../validation/cityValidation/getAll';
 import validUpdate from '../validation/cityValidation/update';
 import validId from '../validation/getByidValid';
 
 const cityRouter = Router();
 
 cityRouter.post('/api/cities', validCreate, new CityController().create);
-cityRouter.get('/api/cities', new CityController().findAll);
+cityRouter.get('/api/cities', validGetAll, new CityController().findAll);
 cityRouter.get('/api/cities/:id', validId, new CityController().getById);
 cityRouter.put('/api/cities/:id', validUpdate, validId, new CityController().update);
 cityRouter.delete('/api/cities/:id', validId, new CityController().delete);
--- src/app/routes/routeClient.ts
+++ src/app/routes/routeClient.ts
@@ -2,12 +2,12 @@ import { Router } from 'express';
 import { ClientController } from '../controller/ClientController';
 import validCreate from '../validation/clientValidation/create';
 import validUpdate from '../validation/clientValidation/update';
-
+import validGetAll from '../validation/clientValidation/getAll';
 
 const clientRouter = Router();
 
 clientRouter.post('/api/clients', validCreate, new ClientController().create);
-clientRouter.get('/api/clients',  new ClientController().findAll);
+clientRouter.get('/api/clients', validGetAll, new ClientController().findAll);
 clientRouter.get('/api/clients/:id', new ClientController().getById);
 clientRouter.put('/api/clients/:id', validUpdate, new ClientController().update);
 clientRouter.delete('/api/clients/:id', new ClientController().delete);
